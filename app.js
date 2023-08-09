fbc2b11a748b2846d20cd1dbfd6e0922a618b602
Debora <escobardebo@gmail.com>
2023-08-09 17:15:43 -0300

    intale express nuevamente por error y con jade, corregí e elimine lo que no se utilizaba

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const productosRouter= require('./routes/productos')
-
-
-const app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/productos', productosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,3 +39,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- controllers/productosController.js
+++ controllers/productosController.js
@@ -1,15 +1,15 @@
 
-const productos = require('../data/productos.json');
+const totalProductos = require('../data/productos.json');
 
 
 module.exports = {
     detail : (req,res) => {
 
         const id = req.params.id;
-        const productoId = productos.find(producto => producto.id === +id);
+        const productos = totalProductos.find(producto => producto.id === +id);
 
         return res.render('detail', {
-            productoId
+            productos
         })
     }
 }
